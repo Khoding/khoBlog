111773e56d42e5fdd497e2efabc777f46d8591a3
Khoding <pcinquantecinq@gmail.com>
2020-10-07 16:28:57 +0200

    formulaires _potables_

--- blog/forms.py
+++ blog/forms.py
@@ -1,12 +1,22 @@
 from django import forms
 
+from markdownx.fields import MarkdownxFormField
+
 from .models import Post, Comment, Category
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'categories', 'description', 'body',)
+        fields = ('title', 'categories', 'description', 'body')
+
+        body = MarkdownxFormField()
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+            'categories': forms.SelectMultiple(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+            'description': forms.Textarea(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+        }
 
 
 class EditForm(forms.ModelForm):
@@ -15,11 +25,27 @@ class EditForm(forms.ModelForm):
         fields = ('title', 'categories', 'description',
                   'body', 'slug', 'private',)
 
+        body = MarkdownxFormField()
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+            'categories': forms.SelectMultiple(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+            'description': forms.Textarea(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+            'slug': forms.TextInput(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+            'private': forms.CheckboxInput(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+        }
+
 
 class CategoryAddForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ('name', 'description', 'private')
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+            'description': forms.Textarea(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+            'private': forms.CheckboxInput(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+        }
 
 
 class CategoryEditForm(forms.ModelForm):
@@ -27,9 +53,22 @@ class CategoryEditForm(forms.ModelForm):
         model = Category
         fields = ('name', 'description', 'slug', 'private')
 
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+            'description': forms.Textarea(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+            'slug': forms.TextInput(attrs={'class': 'w3-input w3-theme-dark w3-border-0'}),
+            'private': forms.CheckboxInput(attrs={'class': 'w3-input w3-theme-dark w3-border-0 w3-border-0'}),
+        }
+
 
 class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
         fields = ('author', 'message')
+
+        message = MarkdownxFormField()
+
+        widgets = {
+            'author': forms.TextInput(attrs={'class': 'w3-input w3-theme-dark w3-border-0 w3-border-0'}),
+        }
