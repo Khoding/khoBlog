215974d1e9a22170c9f30a1c8133bb8cf3ac95f0
Khoding <julien.rutscho@ceff.ch>
2021-01-02 00:14:24 +0100

    region

--- khoBlog/settings.py
+++ khoBlog/settings.py
@@ -252,3 +252,4 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_REGION_NAME = "eu-west-3"
