7650ec73dedf280e6af54019efd74d4f95e4d3bb
Khoding <julien.rutscho@ceff.ch>
2021-03-21 17:28:57 +0100

    req abd cnf.example

--- khoBlog/settings/project/databases_settings.py
+++ khoBlog/settings/project/databases_settings.py
@@ -1,10 +1,6 @@
 from ..django import BASE_DIR
 # https://docs.djangoproject.com/en/stable/ref/settings/#databases
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR + '/db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
