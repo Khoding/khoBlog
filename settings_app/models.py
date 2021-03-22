d174c33218ba585c7b494bfef4af7fe51f595afc
Julien Rutscho <julien.rutscho@ceff.ch>
2021-03-22 16:22:33 +0100

    default theme modifiable in settings and is staff

--- settings_app/models.py
+++ settings_app/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 
 class Settings(models.Model):
+    THEME_CHOICES = [
+        ('default', 'Default'),
+        ('uglybanana', 'Funny Banana'),
+        ('banana', 'Pretty Banana'),
+        ('cherry', 'Hot Cherry'),
+        ('sop', 'Shades Of Purple'),
+        ('leaf', 'Smooth Leaf'),
+        ('nightsky', 'Night Sky'),
+    ]
+
     name = models.CharField(max_length=200)
     shown = models.BooleanField(default=False)
     user = models.ForeignKey(
         'settings_app.UserArea', on_delete=models.CASCADE, related_name='users_area')
     side_menus = models.ManyToManyField(
         'settings_app.SideMenu', related_name='side_menus', blank=True)
+    default_theme = models.CharField(
+        max_length=25, verbose_name="Theme", choices=THEME_CHOICES, default='default')
 
     class Meta:
         verbose_name_plural = "Settings Presets"
@@ -42,7 +54,17 @@ class LinksSideMenu(models.Model):
     VISIBILITY_CHOICES = [
         ('D', 'default'),
         ('NP', 'needs_permission'),
+        ('NS', 'needs_staff'),
         ('NA', 'needs_admin'),
+    ]
+
+    CLASSES_CHOICES = [
+        ('PRI', 'primary'),
+        ('SEC', 'secondary'),
+        ('DAN', 'danger'),
+        ('SUC', 'success'),
+        ('WAR', 'warning'),
+        ('INF', 'info'),
     ]
 
     name = models.CharField(max_length=200, blank=True)
@@ -54,6 +76,8 @@ class LinksSideMenu(models.Model):
         max_length=25, verbose_name="Visibility", choices=VISIBILITY_CHOICES, default='D')
     group = models.ForeignKey(
         'settings_app.LinksGroupSideMenu', on_delete=models.CASCADE, related_name='side_menu_links_group', blank=True, null=True)
+    link_css_classes = models.CharField(
+        max_length=25, verbose_name="Classes", choices=CLASSES_CHOICES, default='PRI')
 
     class Meta:
         verbose_name_plural = "Side Links"
---
+++ settings_app/templatetags/default_app_theme_set.py
@@ -0,0 +1,10 @@
+from django import template
+from settings_app.models import Settings
+
+register = template.Library()
+
+
+@register.inclusion_tag('settings_app/default_app_theme_setting.html')
+def setting(themes):
+    themes = Settings.objects.filter(shown=True)
+    return {'themes': themes}
