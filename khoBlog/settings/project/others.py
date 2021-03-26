3ffa2a0622fd86ba9b5e17741171bf9960a49dcf
Khoding <julien.rutscho@ceff.ch>
2021-03-26 22:11:52 +0100

    Django 3.2 RC1 and dark admin

--- khoBlog/settings/project/others.py
+++ khoBlog/settings/project/others.py
@@ -1,3 +1,4 @@
 FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
 ROOT_URLCONF = 'khoBlog.urls'
 WSGI_APPLICATION = 'khoBlog.wsgi.application'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
