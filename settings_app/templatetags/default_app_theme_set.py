7e8b2ea5d215e0d5a9c86a9a732a74f53331891e
Julien Rutscho <julien.rutscho@ceff.ch>
2021-03-31 09:51:40 +0200

    smol change

--- settings_app/templatetags/default_app_theme_set.py
+++ settings_app/templatetags/default_app_theme_set.py
@@ -8,3 +8,9 @@ register = template.Library()
 def dt_setting(themes):
     themes = Settings.objects.filter(shown=True)
     return {'themes': themes}
+
+
+@register.inclusion_tag('settings_app/all_themes.html')
+def all_theme_setting(themes):
+    themes = Settings.THEME_CHOICES
+    return {'themes': themes}
