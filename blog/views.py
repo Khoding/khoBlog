b680bdf13925749b042c091ff55b37500c700946
Khoding <julien.rutscho@ceff.ch>
2021-04-24 16:13:15 +0200

    list shows things

--- blog/views.py
+++ blog/views.py
@@ -268,7 +268,10 @@ class PostWithTagListView(ListView):
             Tag, slug=self.kwargs['slug'])
         self.title = f'Tag: {self.tags.name}'
         self.description = f'Posts tagged with {self.tags.name}'
-        return self.model.objects.filter(published_date__lte=timezone.now(), withdrawn=False, tags=self.tags).order_by('-published_date')
+        if self.request.user.is_superuser:
+            return self.model.objects.filter(tags=self.tags).order_by('-published_date')
+        else:
+            return self.model.objects.filter(published_date__lte=timezone.now(), withdrawn=False, tags=self.tags).order_by('-published_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
