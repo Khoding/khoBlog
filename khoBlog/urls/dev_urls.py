519dca235af0525d3abf6a15f470bb10bad625c2
Julien Rutscho <julien.rutscho@ceff.ch>
2021-05-03 16:03:33 +0200

    slashes

--- khoBlog/urls/dev_urls.py
+++ khoBlog/urls/dev_urls.py
@@ -12,10 +12,10 @@ app_name = 'dev'
 
 if DEBUG:
     urlpatterns = [
-        path('400', Error400View.as_view(), name='400'),
-        path('403', Error403View.as_view(), name='403'),
-        path('404', Error404View.as_view(), name='404'),
-        path('500', Error500View.as_view(), name='500'),
+        path('400/', Error400View.as_view(), name='400'),
+        path('403/', Error403View.as_view(), name='403'),
+        path('404/', Error404View.as_view(), name='404'),
+        path('500/', Error500View.as_view(), name='500'),
     ]
 else:
     urlpatterns = []
