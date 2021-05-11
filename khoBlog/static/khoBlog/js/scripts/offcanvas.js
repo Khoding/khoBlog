7aac71814568169cf14b541e75ecc559fec98665
Julien Rutscho <julien.rutscho@ceff.ch>
2021-05-11 12:30:55 +0200

    fix offcanvas open

--- khoBlog/static/khoBlog/js/scripts/offcanvas.js
+++ khoBlog/static/khoBlog/js/scripts/offcanvas.js
@@ -3,6 +3,5 @@
 
     document.querySelector('[data-bs-toggle="offcanvas"]').addEventListener('click', function () {
         document.querySelector('.offcanvas-collapse').classList.toggle('open');
-        document.querySelector('body').classList.toggle('offcanvas-open');
     });
 })();
