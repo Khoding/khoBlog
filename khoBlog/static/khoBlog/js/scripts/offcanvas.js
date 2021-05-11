420ec34edf5cc75b46c9e9f03858e952966be6f4
Julien Rutscho <julien.rutscho@ceff.ch>
2021-05-11 13:15:08 +0200

    ok thius

--- khoBlog/static/khoBlog/js/scripts/offcanvas.js
+++ khoBlog/static/khoBlog/js/scripts/offcanvas.js
@@ -3,5 +3,6 @@
 
     document.querySelector('[data-bs-toggle="offcanvas"]').addEventListener('click', function () {
         document.querySelector('.offcanvas-collapse').classList.toggle('open');
+        document.querySelector('body').classList.toggle('offcanvas-open');
     });
 })();
