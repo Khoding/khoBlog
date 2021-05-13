f20c4b8e4ce1670d7ece4ac4fac476f692095cf8
Khoding <julien.rutscho@ceff.ch>
2021-05-13 16:27:25 +0200

    change verbose name for alias_user

--- comments/models.py
+++ comments/models.py
@@ -6,7 +6,7 @@ class CustomComment(CommentAbstractModel):
     title = models.CharField(max_length=200, null=True, blank=True)
     alias_user = models.CharField(max_length=200, null=True, blank=True,
                                   help_text="You can add an Alias Name to your comment if you wish to be "
-                                  'incognito (note that Moderators can still know it\'s you)', verbose_name="Lol")
+                                  'incognito (note that Moderators can still know it\'s you)', verbose_name="alias_name")
 
     def __str__(self):
         return self.title
