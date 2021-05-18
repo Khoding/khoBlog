ab66123be8a812c471117f86c842d72f28636eb5
Julien Rutscho <julien.rutscho@ceff.ch>
2021-05-18 16:24:47 +0200

    cache

--- khoBlog/settings/project/cache.py
+++ khoBlog/settings/project/cache.py
@@ -1,16 +1,16 @@
-# from ..django import DEBUG
+from ..django import DEBUG
 
-# if not DEBUG:
-#     CACHE_MIDDLEWARE_ALIAS = 'default'
-#     CACHE_MIDDLEWARE_SECONDS = 600
-#     CACHE_MIDDLEWARE_KEY_PREFIX = ''
+if not DEBUG:
+    CACHE_MIDDLEWARE_ALIAS = 'default'
+    CACHE_MIDDLEWARE_SECONDS = 600
+    CACHE_MIDDLEWARE_KEY_PREFIX = ''
 
-#     CACHES = {
-#         'default': {
-#             'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-#             'LOCATION': 'cache_table',
-#             'OPTIONS': {
-#                 'MAX_ENTRIES': 300,
-#             }
-#         },
-#     }
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+            'LOCATION': 'cache_table',
+            'OPTIONS': {
+                'MAX_ENTRIES': 300,
+            }
+        },
+    }
