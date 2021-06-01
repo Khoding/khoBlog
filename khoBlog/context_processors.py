10e8edce966781aa221d78fb4b839160cafd59f2
Julien Rutscho <julien.rutscho@ceff.ch>
2021-06-01 10:09:18 +0200

    wtf

--- khoBlog/context_processors.py
+++ khoBlog/context_processors.py
@@ -9,6 +9,7 @@ def context(request):
         'CANONICAL_PATH': request.build_absolute_uri(request.path_info),
         'debug_flag': debug_flag,
         'search_url': reverse('blog:search_results'),
+        'search_title': "Search in Everything",
         'now': timezone.now(),
         'description': "Khodok's Blog was a simple test for Django for school at first before Khodok (the dev of this stupid website) started to like it and continued it as a personal project",
     }
