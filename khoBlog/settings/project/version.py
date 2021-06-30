bb3534a8e2cd1366fb16bb79c501becdadc06958
Khoding <julien.rutscho@ceff.ch>
2021-06-30 14:07:17 +0200

    version handled manually but well

--- khoBlog/settings/project/version.py
+++ khoBlog/settings/project/version.py
@@ -1,6 +1,11 @@
-major = 0
-minor = 4
-patch = 2
+from ..django import BASE_DIR
+
+with open(BASE_DIR + "/version.txt") as v_file:
+    APP_VERSION_NUMBER = v_file.read()
+
+major = int(APP_VERSION_NUMBER.split('.')[0])
+minor = int(APP_VERSION_NUMBER.split('.')[1])
+patch = int(APP_VERSION_NUMBER.split('.')[2])
 
 version_code = major * 1000 + minor * 100 + patch * 10
 version_name = f"{major}.{minor}.{patch}"
