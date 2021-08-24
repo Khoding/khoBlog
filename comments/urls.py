5b0f2b71105ef9f80b1e451b3e7592d064bb9763
Julien Rutscho <julien.rutscho@ceff.ch>
2021-08-24 11:52:59 +0200

    added a comments list page for logged in users

--- comments/urls.py
+++ comments/urls.py
@@ -1,9 +1,11 @@
-from comments.views import CommentUpdateView
+from comments.views import CommentListView, CommentUpdateView
 from django.urls import path
 from django_comments.feeds import LatestCommentFeed
 
 app_name = 'comments'
 urlpatterns = [
+    path('',
+         CommentListView.as_view(), name='comments-list'),
     path('<int:pk>/edit/', CommentUpdateView.as_view(), name='comments-edit'),
     path('rss/',
          LatestCommentFeed(), name='latest_comments_feed'),
--- comments/views.py
+++ comments/views.py
@@ -1,11 +1,29 @@
-from comments.forms import CustomCommentForm
-from django.shortcuts import get_object_or_404
-from django.utils import timezone
-from django.views.generic.edit import CreateView
 from blog.models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from django.views.generic import CreateView, ListView, UpdateView
+
+from comments.forms import CustomCommentForm
+
 from .models import CustomComment
-from django.views.generic import UpdateView
+
+
+class CommentListView(LoginRequiredMixin, ListView):
+    model = CustomComment
+    template_name = 'blog/comments/comment_list.html'
+    context_object_name = 'comment_list'
+    paginate_by = 21
+    paginate_orphans = 5
+
+    def get_queryset(self):
+        return self.model.objects.get_queryset()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Comment List'
+        context['side_title'] = 'Comment List'
+        return context
 
 
 class CommentUpdateView(LoginRequiredMixin, UpdateView):
