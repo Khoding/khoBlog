803795bb2e96866df0324b5992a55ec342bc8f8c
Julien Rutscho <julien.rutscho@ceff.ch>
2021-08-24 11:06:48 +0200

    fixed a scroll bug in posts, still have to fix for comments, added some actions in comments admin

--- comments/admin.py
+++ comments/admin.py
@@ -1,9 +1,29 @@
-from comments.forms import AdminForm
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ngettext
 from django_comments.admin import CommentsAdmin
 
+from comments.forms import AdminForm
+
 from .models import CustomComment
+
+
+def make_site_khoblog(modeladmin, request, queryset):
+    updated = queryset.update(site_id=1)
+    modeladmin.message_user(request, ngettext(
+        '%d comment was successfully added to site khoblog.',
+        '%d comments were successfully added to site khoblog.',
+        updated,
+    ) % updated, messages.SUCCESS)
+
+
+def make_site_localhost(modeladmin, request, queryset):
+    updated = queryset.update(site_id=2)
+    modeladmin.message_user(request, ngettext(
+        '%d comment was successfully added to site localhost.',
+        '%d comments were successfully added to site localhost.',
+        updated,
+    ) % updated, messages.SUCCESS)
 
 
 class CommentsAdmin(CommentsAdmin):
@@ -33,6 +53,9 @@ class CommentsAdmin(CommentsAdmin):
 
     list_filter = ('submit_date', 'site', 'is_public',
                    'is_removed',)
+
+    actions = ["flag_comments", "approve_comments",
+               "remove_comments", make_site_khoblog, make_site_localhost]
 
 
 admin.site.unregister(CustomComment)
