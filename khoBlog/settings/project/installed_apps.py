43ed5c170533484791a2c788d487d2787bb3ab5a
Khoding <julien.rutscho@ceff.ch>
2021-08-31 19:11:12 +0200

    added Github Connection

--- khoBlog/settings/project/installed_apps.py
+++ khoBlog/settings/project/installed_apps.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.discord',
+    'allauth.socialaccount.providers.github',
     'accounts',
 
     # Packages Apps
