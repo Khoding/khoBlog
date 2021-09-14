8f1016ef9518ab0b5febb00533b599e8bbe638da
Khoding <julien.rutscho@ceff.ch>
2021-09-14 21:28:37 +0200

    worked on weblog view and modified post_dates a little

--- blog/templatetags/weblog.py
+++ blog/templatetags/weblog.py
@@ -5,15 +5,17 @@ from ..models import Post
 register = template.Library()
 
 
-@register.inclusion_tag('blog/entry_snippet.html')
-def render_latest_blog_entries(num, summary_first=False, hide_readmore=False, hide_header=False, show_reason='',
+@register.inclusion_tag('blog/object_snippet.html')
+def render_latest_blog_entries(num, summary_first=False, hide_readmore=False, hide_header=False,
                                header_tag=''):
-    entries = Post.objects.get_base_common_queryset()[:num]
+    if not str(num) == "*":
+        entries = Post.objects.get_base_common_queryset()[:num]
+    else:
+        entries = Post.objects.get_base_common_queryset()
     return {
         'entries': entries,
         'summary_first': summary_first,
         'header_tag': header_tag,
-        'show_reason': show_reason,
         'hide_header': hide_header,
         'hide_readmore': hide_readmore,
     }
