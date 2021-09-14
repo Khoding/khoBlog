3880118ad849519257fbb7915452d96da0d274af
Julien Rutscho <julien.rutscho@ceff.ch>
2021-09-14 14:39:30 +0200

    Idk what's wrong now

--- learning_resources/urls.py
+++ learning_resources/urls.py
@@ -1,16 +1,3 @@
-from django.urls import path
-from django.urls.conf import include
+from rest_framework import routers
 
-from .views import (ResourceCreateView, ResourceDeleteView, ResourceDetailView,
-                    ResourceUpdateView)
-
-actions_extra_patterns = [
-    path('', ResourceDetailView.as_view(), name='resource_detail'),
-    path('update/', ResourceUpdateView.as_view(), name='resource_update'),
-    path('delete/', ResourceDeleteView.as_view(), name='resource_delete')
-]
-
-urlpatterns = [
-    path('create/', ResourceCreateView.as_view(), name='resource_create'),
-    path('<pk>/', include(actions_extra_patterns)),
-]
+router = routers.DefaultRouter()
