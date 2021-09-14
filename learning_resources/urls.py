0d27945bc0f143a8126e0c84f8ca005b8033e02a
Julien Rutscho <julien.rutscho@ceff.ch>
2021-09-14 14:48:31 +0200

    roll back last change

--- learning_resources/urls.py
+++ learning_resources/urls.py
@@ -1,3 +1,16 @@
-from rest_framework import routers
+from django.urls import path
+from django.urls.conf import include
 
-router = routers.DefaultRouter()
+from .views import (ResourceCreateView, ResourceDeleteView, ResourceDetailView,
+                    ResourceUpdateView)
+
+actions_extra_patterns = [
+    path('', ResourceDetailView.as_view(), name='resource_detail'),
+    path('update/', ResourceUpdateView.as_view(), name='resource_update'),
+    path('delete/', ResourceDeleteView.as_view(), name='resource_delete')
+]
+
+urlpatterns = [
+    path('create/', ResourceCreateView.as_view(), name='resource_create'),
+    path('<pk>/', include(actions_extra_patterns)),
+]
