91a6472bde5262463fe06499681de15437b1b621
Julien Rutscho <julien.rutscho@ceff.ch>
2021-09-21 13:05:56 +0200

    dot

--- khoBlog/settings/third_party/comments.py
+++ khoBlog/settings/third_party/comments.py
@@ -6,7 +6,7 @@ COMMENTS_DISABLED_GLOBALLY = env.bool('COMMENTS_DISABLED_GLOBALLY')
 
 if not COMMENTS_USE_OLD_SYSTEM:
     COMMENTS_APP = 'django_comments_xtd'
-    COMMENTS_XTD_CONFIRM_EMAIL = True
+    COMMENTS_XTD_CONFIRM_EMAIL = False
     COMMENTS_XTD_MODEL = 'comments.models.CustomCommentXTD'
     COMMENTS_XTD_FORM_CLASS = 'comments.forms.CustomCommentXTDForm'
     COMMENTS_XTD_MAX_THREAD_LEVEL = 5
