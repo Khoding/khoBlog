f5693d9c0a8bd95b3ce1450d0451d60556d633cd
Khoding <julien.rutscho@ceff.ch>
2021-09-25 14:59:30 +0200

    const

--- khoBlog/static/khoBlog/js/scripts/dynamic.js
+++ khoBlog/static/khoBlog/js/scripts/dynamic.js
@@ -1,8 +1,8 @@
 /* jshint esversion: 6 */
-let isRoot = location.pathname === '/' && location.hash === '' && location.search === '';
+const isRoot = location.pathname === '/' && location.hash === '' && location.search === '';
 
 if (isRoot) {
-    let ele = document.getElementsByClassName('linkHome');
+    const ele = document.getElementsByClassName('linkHome');
     for (let i = 0; i < ele.length; i++) {
         ele[i].classList.toggle('isHome');
     }
