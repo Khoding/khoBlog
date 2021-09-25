ff5439eb3e99bde21c95e0b678a83203932b86ac
Khoding <julien.rutscho@ceff.ch>
2021-09-25 14:54:18 +0200

    antipattern

--- khoBlog/static/khoBlog/js/scripts/dynamic.js
+++ khoBlog/static/khoBlog/js/scripts/dynamic.js
@@ -1,8 +1,9 @@
-var isRoot = location.pathname === '/' && location.hash === '' && location.search === '';
+/* jshint esversion: 6 */
+let isRoot = location.pathname === '/' && location.hash === '' && location.search === '';
 
 if (isRoot) {
-    var ele = document.getElementsByClassName('linkHome');
-    for (var i = 0; i < ele.length; i++) {
+    let ele = document.getElementsByClassName('linkHome');
+    for (let i = 0; i < ele.length; i++) {
         ele[i].classList.toggle('isHome');
     }
 }
