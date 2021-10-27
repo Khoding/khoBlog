ae9f2a0fc32e19ce60022ef07bdeb35db4b218c2
Khoding <julien.rutscho@ceff.ch>
2021-10-27 13:11:43 +0200

    Filters are broken Fixes #27

--- blog/templatetags/paginator_tags.py
+++ blog/templatetags/paginator_tags.py
@@ -9,3 +9,15 @@ def get_proper_elided_page_range(p, number, on_each_side=3, on_ends=2):
     """Paginator get_proper_elided_page_range override"""
     paginator = Paginator(p.object_list, p.per_page, p.orphans)
     return paginator.get_elided_page_range(number=number, on_each_side=on_each_side, on_ends=on_ends)
+
+
+@register.simple_tag
+def relative_url(value, field_name, urlencode=None):
+    url = "?{}={}".format(field_name, value)
+
+    if urlencode:
+        querystring = urlencode.split("&")
+        filtered_querystring = filter(lambda p: p.split("=")[0] != field_name, querystring)
+        encoded_querystring = "&".join(filtered_querystring)
+        url = "{}&{}".format(url, encoded_querystring)
+    return url
