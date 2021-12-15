3b2f68ae48c2f425c05ee3f4e887c42586d1dc2d
Khoding <julien.rutscho@ceff.ch>
2021-12-15 14:49:03 +0100

    removed some unused files

--- blog/templatetags/post_cards.py
+++ blog/templatetags/post_cards.py
@@ -4,9 +4,10 @@ register = template.Library()
 
 
 @register.inclusion_tag("blog/base_post_card.html", takes_context=True)
-def render_cards(context, o, in_list=False):
+def render_cards(context, o, is_featured=False, in_list=False):
     return {
         "context": context,
         "object": o,
+        "is_featured": is_featured,
         "in_list": in_list,
     }
