9e523a83b37be6bdcd9c240f00d998c2176035bd
Khoding <julien.rutscho@ceff.ch>
2022-02-05 14:18:56 +0100

    post_count only 1 field

--- blog/templatetags/post_count.py
+++ blog/templatetags/post_count.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.simple_tag()
 def o_count(o):
-    return o.model.objects.filter(is_removed=False).count()
+    return o.model.objects.only("pk").filter(is_removed=False).count()
