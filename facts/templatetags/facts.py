ffb308a1c05af5ad64b251e3942194d629c2be27
Khoding <julien.rutscho@ceff.ch>
2022-04-07 07:55:24 +0200

    specific date facts bacc

--- facts/templatetags/facts.py
+++ facts/templatetags/facts.py
@@ -75,3 +75,55 @@ def facts(context, hide_header=False, hide_readmore=False):
                 "timesince": timesince,
                 "context": context,
             }
+
+
+@register.inclusion_tag("tailwind/display_fact.html")
+def tw_facts():
+    """Facts tag"""
+    specific_date_fact_count = SpecificDateFact.objects.filter(
+        (
+            Q(
+                showing_date__day=datetime.today().day,
+                showing_date__month=datetime.today().month,
+                showing_rule="D",
+            )
+            | Q(showing_date__hour=datetime.now().hour, showing_rule="T")
+            | Q(
+                showing_date__day=datetime.today().day,
+                showing_date__month=datetime.today().month,
+                showing_date__hour=datetime.now().hour,
+                showing_rule="DT",
+            )
+        )
+        & (Q(is_recurrent=True) | Q(is_recurrent=False, showing_date__year=datetime.now().year))
+    ).count()
+    if specific_date_fact_count != 0:
+        facts = SpecificDateFact.objects.filter(shown=True).filter(
+            (
+                Q(
+                    showing_date__day=datetime.today().day,
+                    showing_date__month=datetime.today().month,
+                    showing_rule="D",
+                )
+                | Q(showing_date__hour=datetime.now().hour, showing_rule="T")
+                | Q(
+                    showing_date__day=datetime.today().day,
+                    showing_date__month=datetime.today().month,
+                    showing_date__hour=datetime.now().hour,
+                    showing_rule="DT",
+                )
+            )
+            & (Q(is_recurrent=True) | Q(is_recurrent=False, showing_date__year=datetime.now().year))
+        )
+        if facts.count() > 0:
+            o = facts.order_by("?")[0]
+            o.rnd_chosen()
+            is_old_enough = False
+            timesince = datetime.now().year - o.showing_date.year
+            if timesince > 0:
+                is_old_enough = True
+            return {
+                "object": o,
+                "is_old_enough": is_old_enough,
+                "timesince": timesince,
+            }
