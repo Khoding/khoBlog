455fd9695f4e8aebeb7d4a7fc2622f63fcc587cf
Khoding <julien.rutscho@ceff.ch>
2022-04-26 10:49:10 +0200

    versionning

--- khoBlog/settings/project/version.py
+++ khoBlog/settings/project/version.py
@@ -8,13 +8,10 @@ minor = int(APP_VERSION_NUMBER.split(".")[1])
 patch = int(APP_VERSION_NUMBER.split(".")[2])
 build = APP_VERSION_NUMBER.split(".")[3]
 
-if isinstance(build, str):
-    ver_code = f"{major * 10000 + minor * 1000 + patch * 100}{build}"
-else:
-    ver_code = f"{major * 10000 + minor * 1000 + patch * 100 + build}"
+ver_code = f"{major * 10000 + minor * 1000 + patch * 100}-{build}"
 
 version_code = ver_code
-version_name = f"{major}.{minor}.{patch}-{build}"
+version_name = f"v{major}.{minor}.{patch}-{build}"
 
 __version__ = version_name
 
