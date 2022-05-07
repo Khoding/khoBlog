3bcac5fe1839c4099fb8c04e952f795f8f1b1f5a
Khoding <julien.rutscho@ceff.ch>
2022-05-07 13:32:38 +0200

    Version fix and code a bit more logical

--- khoBlog/settings/project/version.py
+++ khoBlog/settings/project/version.py
@@ -15,7 +15,10 @@ ver = re.split(
 version = ver[3:8]
 
 
-ver_code = f"{int(version[1]) * 10000 + int(version[2]) * 1000 + int(version[3]) * 100}-{version[4]}"
+ver_code = (
+    f"{int(version[1]) * 10000 + int(version[2]) * 1000 + int(version[3]) * 100}"
+    + ("", f"-{version[4]}")[version[4] != None]
+)
 
 version_name = APP_VERSION_NUMBER
 version_code = ver_code
@@ -23,6 +26,6 @@ version_code = ver_code
 __version__ = version_name
 
 APP_VERSION_NAME = __version__
-APP_VERSION_NUMBER = version_code
+APP_VERSION_CODE = version_code
 
-FULL_VERSION = f"{APP_VERSION_NAME} {u'–'} {APP_VERSION_NUMBER}"
+FULL_VERSION = f"{APP_VERSION_NAME} {u'–'} {APP_VERSION_CODE}"
