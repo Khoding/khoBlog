54ef3616f145358bba73cd1e6bf2b17a11950e21
Khoding <julien.rutscho@ceff.ch>
2022-06-16 11:58:04 +0200

    recursive styles

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -1,9 +1,4 @@
-/**
- * This is a minimal config.
- *
- * If you need the full config, get it from here:
- * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
- */
+/** @type {import('tailwindcss').Config} */
 
 module.exports = {
     darkMode: "class",
@@ -91,11 +86,6 @@ module.exports = {
         extend: {},
     },
     plugins: [
-        /**
-         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
-         * for forms. If you don't like it or have own styling for forms,
-         * comment the line below to disable '@tailwindcss/forms'.
-         */
         require("@tailwindcss/forms"),
         require("@tailwindcss/typography"),
         require("@tailwindcss/line-clamp"),
