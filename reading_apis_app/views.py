a7bd08a7561e8a8cd9e150ab567a751dedd18eca
Khoding <julien.rutscho@ceff.ch>
2022-06-18 13:45:03 +0200

    (PYL-R1710) Inconsistent return statements this should finally fix it Fixes #132

--- reading_apis_app/views.py
+++ reading_apis_app/views.py
@@ -11,16 +11,13 @@ def return_the_api(request):
     api = request.GET["e"]
     response = requests.get(api)
     thing = response.json()
+    f = "api"
     if "format" in request.GET:
         f = request.GET["format"]
-        if f == "json":
-            context = {"things": thing}
-            return context
-        if f == "api" or f is None or f == "":
-            template = "reading_apis_app/list.html"
-            context = {"things": thing, "title": "API Reading App"}
-            return render(request, template, context)
-    else:
+    if f == "json":
+        context = {"things": thing}
+        return context
+    if f == "api":
         template = "reading_apis_app/list.html"
         context = {"things": thing, "title": "API Reading App"}
         return render(request, template, context)
@@ -34,16 +31,13 @@ def return_the_api_detail(request):
     api = request.GET["e"]
     response = requests.get(api)
     thing = response.json()
+    f = "api"
     if "format" in request.GET:
         f = request.GET["format"]
-        if f == "json":
-            context = {"things": thing}
-            return context
-        if f == "api" or f is None or f == "":
-            template = "reading_apis_app/detail.html"
-            context = {"things": thing, "title": "API Reading App"}
-            return render(request, template, context)
-    else:
+    if f == "json":
+        context = {"things": thing}
+        return context
+    if f == "api":
         template = "reading_apis_app/detail.html"
         context = {"things": thing, "title": "API Reading App"}
         return render(request, template, context)
