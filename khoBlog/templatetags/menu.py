d126a3436247b0bcc50770136b58a2dc3c26a653
Khoding <julien.rutscho@ceff.ch>
2022-08-08 17:35:46 +0200

    Deepsource fixes

--- khoBlog/templatetags/menu.py
+++ khoBlog/templatetags/menu.py
@@ -3,13 +3,16 @@ from django import template
 
 register = template.Library()
 
+
 # inclusion tag for mega menu
 @register.inclusion_tag("tailwind/mega_menu.html", takes_context=True)
 def mega_menu(context, menu):
+    """Mega menu tag"""
     return {"user": context.request.user, "menu": menu}
 
 
 # inclusion tag for menu tab
 @register.inclusion_tag("tailwind/menu_sections_mobile.html", takes_context=True)
 def tab_menu(context, menu):
+    """Tab menu tag"""
     return {"user": context.request.user, "menu": menu}
