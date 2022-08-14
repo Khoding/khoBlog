bbb662a4033089534b62b36c3c75cbd2a9229118
Khoding <julien.rutscho@ceff.ch>
2022-08-14 18:03:10 +0200

    hide block titles in menu when there's no link

--- settings_app/templatetags/menu_footer_set.py
+++ settings_app/templatetags/menu_footer_set.py
@@ -10,15 +10,21 @@ def mfl_setting(context, link_type):
     """Menu footer links tag"""
     has_perms = context.get("perms", ["settings_app.view_menu_footer_link"])
     menu_footer_links = ""
+    block_title = ""
     if link_type == "D":
         menu_footer_links = MenuFooterLink.objects.filter(visibility="D")
+        block_title = "Direct links"
     if context.request.user.is_authenticated:
         if link_type == "NP" and has_perms:
             menu_footer_links = MenuFooterLink.objects.filter(visibility="NP")
+            block_title = "DL Permission"
         elif link_type == "NS" and context.request.user.is_staff:
             menu_footer_links = MenuFooterLink.objects.filter(visibility="NS")
+            block_title = "DL Staff"
         elif link_type == "NA" and context.request.user.is_superuser:
             menu_footer_links = MenuFooterLink.objects.filter(visibility="NA")
+            block_title = "DL Admin"
         elif link_type == "NAL" and context.request.user.is_superuser:
             menu_footer_links = MenuFooterLink.objects.filter(visibility="NAL")
-    return {"menu_footer_links": menu_footer_links}
+            block_title = "DL Admin Lists"
+    return {"menu_footer_links": menu_footer_links, "block_title": block_title}
