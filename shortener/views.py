2e137028ae72175635ec51af1bf19c01355108f3
Khoding <julien.rutscho@ceff.ch>
2022-09-14 16:06:51 +0200

    Sometimes you make bad choices

--- shortener/views.py
+++ shortener/views.py
@@ -1,4 +1,3 @@
-from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import get_object_or_404, redirect
 
 from .models import URL
@@ -6,8 +5,6 @@ from .models import URL
 
 def short_redirect(request, slug):
     """Redirect to the permanent URL of a link"""
-    current_site = get_current_site(request)
     url = get_object_or_404(URL, slug=slug)
     url.clicked()
-    redirect_url = f"{request.scheme}://{current_site.domain}{url.full_url}"
-    return redirect(redirect_url)
+    return redirect(url.full_url)
