bb646d3cf8982608fdabe3906247d558fcdc4a7b
Julien Rutscho <58421711+Khoding@users.noreply.github.com>
2022-09-22 10:56:17 +0200

    Scrollbar in changelog

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -103,5 +103,6 @@ module.exports = {
         require("@tailwindcss/typography"),
         require("@tailwindcss/line-clamp"),
         require("@tailwindcss/aspect-ratio"),
+        require("tailwind-scrollbar"),
     ],
 };
