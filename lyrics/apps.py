a13f6f90d460759b9388871132bc87cc1bb60311
Khoding <julien.rutscho@ceff.ch>
2023-01-07 17:27:53 +0100

    (PY-D0002) Missing class docstring Fixes #187

--- lyrics/apps.py
+++ lyrics/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class LyricsConfig(AppConfig):
+    """LyricsConfig App Config"""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "lyrics"
